81370f8474f9a3b43abd0240b3f39bf43ffc88a3
OOjeser <boss.gavrilenko@bk.ru>
2021-12-15 10:54:39 +0500

    lab3

---
+++ LR3_GavrilenkoSergey/Form1.cs
@@ -0,0 +1,10 @@
+namespace LR3_GavrilenkoSergey
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LR3_GavrilenkoSergey/Program.cs
@@ -0,0 +1,15 @@
+namespace LR3_GavrilenkoSergey
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
